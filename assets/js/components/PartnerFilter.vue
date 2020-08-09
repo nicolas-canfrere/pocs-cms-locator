2041f4819e7707de97d6d0ccec3795e40dcfc9b1
Nicolas Canfrere <nicolas@myprm.com>
2020-08-09 12:44:48 +0200

    save

--- assets/js/components/PartnerFilter.vue
+++ assets/js/components/PartnerFilter.vue
@@ -1,56 +1,91 @@
 <template>
 	<div class="partner__filter" :class="{open: openMenu}">
 		<div class="filter__section" v-show="categories.length > 1">
-			<div class="section__title">Catégories</div>
-			<div class="section__content">
-				<ul class="unstyled p-0 mb-2">
-					<li v-for="(category, index) in categories" :key="category.id" class="mb-2">
-						<div class="checkbox">
-							<label>
-								<input type="checkbox" v-model="category.selected" @change="toggleFilterCategory(category)">
-								<span class="checkbox__square">
+			<div class="section__title" @click="categorySectionOpen = !categorySectionOpen"
+				 :class="{'section-open': categorySectionOpen}">
+				Catégories
+				<i class="section__title_arrow fas fa-chevron-down"></i>
+			</div>
+			<transition
+					name="expand-filter-section"
+					@enter="accordionEnter"
+					@after-enter="accordionAfterEnter"
+					@leave="accordionLeave"
+			>
+				<div class="section__content" v-show="categorySectionOpen">
+					<ul class="unstyled p-0 mb-2">
+						<li v-for="(category, index) in categories" :key="category.id" class="mb-2">
+							<div class="checkbox">
+								<label>
+									<input type="checkbox" v-model="category.selected"
+										   @change="toggleFilterCategory(category)">
+									<span class="checkbox__square">
 									<span class="checkbox__check"></span>
 								</span>
-								<span class="checkbox__label">{{category.name}}</span>
-							</label>
-						</div>
-					</li>
-				</ul>
-				<a href="#" @click.prevent="toggleAllCategories" class="select-all">
-					<span v-if="allCategoriesSelected === false">Tout séléctionner</span>
-					<span v-else>Tout déséléctionner</span>
-				</a>
-			</div>
+									<span class="checkbox__label">{{category.name}}</span>
+								</label>
+							</div>
+						</li>
+					</ul>
+					<a href="#" @click.prevent="toggleAllCategories" class="select-all">
+						<span v-if="allCategoriesSelected === false">Tout séléctionner</span>
+						<span v-else>Tout déséléctionner</span>
+					</a>
+				</div>
+			</transition>
+
 		</div>
 		<div class="filter__section" v-show="countries.length > 1">
-			<div class="section__title">Localisation</div>
-			<div class="section__content">
-				<multiselect
-					:value="filters.countries"
-					:options="countries"
-					:multiple="true"
-					:close-on-select="false"
-					:clear-on-select="false"
-					:preserve-search="false"
-					placeholder="Rechercher..."
-					label="label"
-					track-by="id"
-					:preselect-first="false"
-					:taggable="true"
-					@select="addCountry"
-					@remove="removeCountry"
-				>
-				</multiselect>
+			<div class="section__title" @click="countrySectionOpen = !countrySectionOpen"
+				 :class="{'section-open': countrySectionOpen}">
+				Localisation
+				<i class="section__title_arrow fas fa-chevron-down"></i>
 			</div>
+			<transition
+					name="expand-filter-section"
+					@enter="accordionEnter"
+					@after-enter="accordionAfterEnter"
+					@leave="accordionLeave"
+			>
+				<div class="section__content" v-show="countrySectionOpen">
+					<multiselect
+							:value="filters.countries"
+							:options="countries"
+							:multiple="true"
+							:close-on-select="false"
+							:clear-on-select="false"
+							:preserve-search="false"
+							placeholder="Rechercher..."
+							label="label"
+							track-by="id"
+							:preselect-first="false"
+							:taggable="true"
+							@select="addCountry"
+							@remove="removeCountry"
+					>
+					</multiselect>
+				</div>
+			</transition>
 		</div>
 		<div class="filter__section">
-			<div class="section__title">Rechercher quelque chose</div>
-			<div class="section__content">
-				<div class="search">
-					<input type="text">
-					<div class="icon"><i class="fas fa-search"></i></div>
-				</div>
+			<div class="section__title" @click="searchSectionOpen = !searchSectionOpen"
+				 :class="{'section-open': searchSectionOpen}">
+				Rechercher quelque chose
+				<i class="section__title_arrow fas fa-chevron-down"></i>
 			</div>
+			<transition
+					name="expand-filter-section"
+					@enter="accordionEnter"
+					@after-enter="accordionAfterEnter"
+					@leave="accordionLeave"
+			>
+				<div class="section__content" v-show="searchSectionOpen">
+					<div class="search">
+						<input type="text">
+						<div class="icon"><i class="fas fa-search"></i></div>
+					</div>
+				</div>
+			</transition>
 		</div>
 	</div>
 </template>
@@ -58,29 +93,52 @@
 <script>
   import Multiselect from 'vue-multiselect'
   import { mapGetters, mapState } from 'vuex'
+
   export default {
     name: 'PartnerFilter',
-	components: { Multiselect },
-	props: ['openMenu'],
-	data() {
+    components: { Multiselect },
+    props: ['openMenu'],
+    data () {
       return {
-        allCategoriesSelected: false
-	  }
-	},
-	computed: {
+        allCategoriesSelected: false,
+        categorySectionOpen: true,
+        countrySectionOpen: true,
+        searchSectionOpen: true,
+      }
+    },
+    computed: {
       ...mapGetters({
         categories: 'allCategories',
-		countries: 'allCountries'
-	  }),
-	  ...mapState(['filters'])
-	},
-	methods: {
+        countries: 'allCountries'
+      }),
+      ...mapState(['filters'])
+    },
+    methods: {
+      accordionEnter (element) {
+        element.style.height = 'auto'
+        const height = getComputedStyle(element).height
+        element.style.height = 0
+        getComputedStyle(element)
+        setTimeout(() => {
+          element.style.height = height
+        })
+      },
+      accordionAfterEnter (element) {
+        element.style.height = 'auto'
+      },
+      accordionLeave (element) {
+        element.style.height = getComputedStyle(element).height
+        getComputedStyle(element)
+        setTimeout(() => {
+          element.style.height = 0
+        })
+      },
       toggleFilterCategory (category) {
-		this.$store.dispatch('toggleCategoryToFilters', category)
-	  },
-	  addCountry (country) {
-		this.$store.dispatch('addCountryToFilters', country)
-	  },
+        this.$store.dispatch('toggleCategoryToFilters', category)
+      },
+      addCountry (country) {
+        this.$store.dispatch('addCountryToFilters', country)
+      },
       removeCountry (country) {
         this.$store.dispatch('removeCountryToFilters', country)
       },
@@ -90,16 +148,16 @@
           this.categories.forEach(el => {
             el.selected = false
           })
-		  this.$store.dispatch('unselectAllCategoriesToFilters')
-		} else {
+          this.$store.dispatch('unselectAllCategoriesToFilters')
+        } else {
           this.allCategoriesSelected = true
           this.categories.forEach(el => {
             el.selected = true
           })
           this.$store.dispatch('selectAllCategoriesToFilters')
-		}
+        }
 
-	  }
-	}
+      }
+    }
   }
 </script>
